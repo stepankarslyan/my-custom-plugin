736e67d8c669e832670971491647874c09ead807
Stepan Karslyan <stepan.karslyan@gmail.com>
2014-07-01 10:13:49 +0400

    creating plugin header

---
+++ main.php
@@ -0,0 +1,10 @@
+<?php 
+    /*
+    Plugin Name: External JS libreries loader
+    Plugin URI: http://www.github.com/stepankarslyan/my-custom-plugin.git
+    Description: Plugin for loading client-side javascript files into WP
+    Author: S. Gharslyan
+    Version: 0.0.1
+    Author URI: http://www.github.com/stepankarslyan
+    */
+?>
